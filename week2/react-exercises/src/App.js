6ead43db7b31ae1fd079f736fed5a6e9eebbfc2c
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-14 14:56:54 +0100

    added ex-1 in react-exercises folder

---
+++ week2/react-exercises/src/App.js
@@ -0,0 +1,13 @@
+import Friend from './new-friend'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <Friend />
+     <hr></hr>
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/react-exercises/src/new-friend.js
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+
+ const Friend = () => {
+ const [friend, setFriend] = useState([]);
+ const [hasEroor, setError] = useState(false);
+ 
+ function GetFriend () {
+    const url = 'https://www.randomuser.me/api?results=1';
+    fetch (url)
+    .then(res => res.json())
+    .then (data => setFriend(data.results[0]))
+    .catch (err => {
+        setError(true)
+        console.error(err)
+    });
+ };
+
+ return (
+     <div>
+        <Button fetch= {GetFriend} />
+        <FriendProfile friendProps= {{
+            friend: friend,
+            error: hasEroor
+        }} />
+    </div>
+ )
+};
+
+const FriendProfile = ({friendProps}) => {
+    const {friend, error} = friendProps;
+    console.log(friend)
+    return (
+        <div className= 'friend_container'>
+            {friend.name &&
+            <ul>
+                <li>First Name: {friend.name.first}</li>
+                <li>Last Name: {friend.name.last}</li>
+                <li>Address: {friend.location.street.name},{friend.location.street.number}</li>
+                <li>Country: {friend.location.country}</li>
+                <li>Email: {friend.email}</li>
+                <li>Phone: {friend.cell}</li>
+            </ul>
+            }
+            {error && <h1> We were not able to process your request "shit happens dude" </h1> }
+        </div>
+    )
+};
+
+const Button = (props) => {
+    return <button className='btn' onClick={props.fetch}> Get a friend! </button>
+};
+export default Friend
