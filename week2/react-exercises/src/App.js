b24780ec913babf96633c9acaa7b6a590072f94e
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-16 01:34:39 +0100

    added random-joke.js

--- week2/react-exercises/src/App.js
+++ week2/react-exercises/src/App.js
@@ -1,11 +1,14 @@
 import Friend from './new-friend';
 import DogGallery from './dog-photo';
+import RandomJoke from './random-joke';
 
 function App() {
   
   return (
     <div className="App">
      <Friend />
+     <hr></hr>
+     <RandomJoke/>
      <hr></hr>
      <DogGallery/>
     </div>
---
+++ week2/react-exercises/src/random-joke.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+
+const RandomJoke = () => {
+    const [joke, setJoke] = useState({});
+    const [error, serError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    
+    const GetData = () => {
+        setLoading(true);
+        const url = 'https://official-joke-api.appspot.com/random_joke';
+        fetch (url)
+        .then (res => res.json())
+        .then (data => {
+            setJoke(data)
+        })
+        .catch (err => {
+            console.error(err)
+            serError(true)
+        })
+        .finally(() => setLoading(false))
+        } 
+    
+    useEffect(() => {
+        GetData()
+        },[])
+
+    return (
+        <div>     
+            <Joke props= {{
+                joke: joke,
+                loading: loading,
+                error: error
+            }} />
+        </div>
+    )
+};
+
+
+const Joke = ({props}) => {
+    return (
+        <div>
+            {props.loading && <h4>Loeading...</h4>}
+            {props.error 
+             ? <h2> Sorry! we were not able to process your request </h2> 
+             : <> <p> setup: {props.joke.setup}</p> <p>punchline: {props.joke.punchline} </p> 
+              </>}
+        </div>
+        
+    )
+}
+
+export default RandomJoke;
