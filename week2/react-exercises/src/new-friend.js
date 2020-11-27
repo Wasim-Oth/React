94428764c1810fe88d0591781449fa024c896a58
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-27 20:41:05 +0100

    updated new-friend.js after the feedback

--- week2/react-exercises/src/new-friend.js
+++ week2/react-exercises/src/new-friend.js
@@ -1,53 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
- const Friend = () => {
- const [friend, setFriend] = useState([]);
- const [hasEroor, setError] = useState(false);
- 
- function GetFriend () {
-    const url = 'https://www.randomuser.me/api?results=1';
-    fetch (url)
-    .then(res => res.json())
-    .then (data => setFriend(data.results[0]))
-    .catch (err => {
-        setError(true)
-        console.error(err)
-    });
- };
+const Friend = () => {
+  const [friend, setFriend] = useState([]);
+  const [hasError, setError] = useState(false);
 
- return (
-     <div>
-         <h1>Exercise 1: New friend on demand</h1>
-        <Button fetch= {GetFriend} />
-        <FriendProfile friendProps= {{
-            friend: friend,
-            error: hasEroor
-        }} />
+  function GetFriend() {
+    const url = "https://www.randomuser.me/api?results=1";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data.results[0]))
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      });
+  }
+
+  return (
+    <div>
+      <h1>Exercise 1: New friend on demand</h1>
+      <Button fetch={GetFriend} />
+      <FriendProfile
+        friendProps={{
+          friend,
+          hasError,
+        }}
+      />
     </div>
- )
+  );
 };
 
-const FriendProfile = ({friendProps}) => {
-    const {friend, error} = friendProps;
-    console.log(friend)
-    return (
-        <div className= 'friend_container'>
-            {friend.name &&
-            <ul>
-                <li>First Name: {friend.name.first}</li>
-                <li>Last Name: {friend.name.last}</li>
-                <li>Address: {friend.location.street.name},{friend.location.street.number}</li>
-                <li>Country: {friend.location.country}</li>
-                <li>Email: {friend.email}</li>
-                <li>Phone: {friend.cell}</li>
-            </ul>
-            }
-            {error && <h1> We were not able to process your request "shit happens dude" </h1> }
-        </div>
-    )
+const FriendProfile = ({ friendProps }) => {
+  const { friend, hasError } = friendProps;
+  const { name, location, email, cell } = friend;
+  return (
+    <div className="friend_container">
+      {name && (
+        <ul>
+          <li>First Name: {name.first}</li>
+          <li>Last Name: {name.last}</li>
+          <li>
+            Address: {location.street.name},{location.street.number}
+          </li>
+          <li>Country: {location.country}</li>
+          <li>Email: {email}</li>
+          <li>Phone: {cell}</li>
+        </ul>
+      )}
+      {hasError && (
+        <h1> We were not able to process your request "shit happens dude" </h1>
+      )}
+    </div>
+  );
 };
 
 const Button = (props) => {
-    return <button className='btn' onClick={props.fetch}> Get a friend! </button>
+  return (
+    <button className="btn" onClick={props.fetch}>
+      Get a friend!
+    </button>
+  );
 };
-export default Friend
+export default Friend;
