1e1c885d5e20bfe785914ddb2dbad22ef377f093
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-27 20:47:09 +0100

    updated random-joke.js after the feedback

--- week2/react-exercises/src/random-joke.js
+++ week2/react-exercises/src/random-joke.js
@@ -1,54 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 const RandomJoke = () => {
-    const [joke, setJoke] = useState({});
-    const [error, serError] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [joke, setJoke] = useState({});
+  const [hasError, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    
-    const GetData = () => {
-        setLoading(true);
-        const url = 'https://official-joke-api.appspot.com/random_joke';
-        fetch (url)
-        .then (res => res.json())
-        .then (data => {
-            setJoke(data)
-        })
-        .catch (err => {
-            console.error(err)
-            serError(true)
-        })
-        .finally(() => setLoading(false))
-        } 
-    
-    useEffect(() => {
-        GetData()
-        },[])
+  const GetData = () => {
+    setLoading(true);
+    const url = "https://official-joke-api.appspot.com/random_joke";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setJoke(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  };
 
-    return (
-        <div>     
-            <Joke props= {{
-                joke: joke,
-                loading: loading,
-                error: error
-            }} />
-        </div>
-    )
+  useEffect(() => {
+    GetData();
+  }, []);
+
+  return (
+    <div>
+      <Joke
+        props={{
+          joke,
+          loading,
+          hasError
+        }}
+      />
+    </div>
+  );
 };
 
-
-const Joke = ({props}) => {
-    return (
-        <div>
-            <h1>Exercise 3: Random Joke Generator</h1>
-            {props.loading && <h4>Loeading...</h4>}
-            {props.error 
-             ? <h2> Sorry! we were not able to process your request </h2> 
-             : <> <p> setup: {props.joke.setup}</p> <p>punchline: {props.joke.punchline} </p> 
-              </>}
-        </div>
-        
-    )
-}
+const Joke = ({ props }) => {
+  const { joke, loading, hasError } = props;
+  return (
+    <div>
+      <h1>Exercise 3: Random Joke Generator</h1>
+      {loading && <span> Loading...</span>}
+      {hasError ? (
+        <h2> Sorry! we were not able to process your request </h2>
+      ) : (
+        <>
+          <p> setup: {joke.setup} </p>
+          <p> punchline: {joke.punchline} </p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default RandomJoke;
