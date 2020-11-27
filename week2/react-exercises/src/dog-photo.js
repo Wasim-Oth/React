9972417470dc55050c57855c02fbf0c49049e84e
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-27 20:28:26 +0100

    updated dog-photo.js after the feedback

--- week2/react-exercises/src/dog-photo.js
+++ week2/react-exercises/src/dog-photo.js
@@ -1,49 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const DogGallery = () => {
-    const [dogPhotos, setDogPhotos] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [Error, setError] = useState(false);
+  const [dogPhotos, setDogPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasError, setError] = useState(false);
+  let photosKey = 1;
 
-    const getDogPhoto = () => {
-        setLoading(true)
-        const url = 'https://dog.ceo/api/breeds/image/random';
-        fetch (url)
-        .then (res => res.json())
-        .then (data => setDogPhotos([...dogPhotos, data.message]))
-        .catch (err => {
-            console.log(err)
-            setError (true)
-        })
-        .finally (() => setLoading(false))
-    };
+  const getDogPhoto = () => {
+    setLoading(true);
+    const url = "https://dog.ceo/api/breeds/image/random";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setDogPhotos([...dogPhotos, data.message]))
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  };
 
-    return(
-        <div>
-            <h1>Exercise 2: Dog photo gallery</h1>
-            <Button fetch= {getDogPhoto} />
-            <DogPhoto props= {{
-                dogPhotos: dogPhotos,
-                loading: loading,
-                Error: Error
-                }} />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Exercise 2: Dog photo gallery</h1>
+      <Button fetch={getDogPhoto} />
+      {loading && <h4> Loading...</h4>}
+      {hasError && <h2> Sorry! we were not able to process your request </h2>}
+
+      {!hasError && dogPhotos.length === 0 && (
+        <h3> Get your first dog by clicking the button! </h3>
+      )}
+
+      {dogPhotos.map((dog) => {
+        return (
+          <DogPhoto src={dog} key={photosKey++} alt="dog" />
+        );
+      })}
+    </div>
+  );
 };
 
-const DogPhoto = ({props}) => {
-    return (
-        <div>
-            {props.loading && <h4> Loading...</h4>}
-            {props.Error && <h2> Sorry! we were not able to process your request </h2>}
-            { (!props.Error && props.dogPhotos.length === 0) && <h3> Get your first dog by clicking the button! </h3> } 
-            { (!props.Error && props.dogPhotos)  && props.dogPhotos.map((dog, index) => <img src={dog} key={index} alt='dog' />) }
-        </div>
-    )
+const DogPhoto = ({ src }) => {
+  return (
+    <div>
+      <img src={src} alt="doggi" />
+    </div>
+  );
 };
 
 const Button = (props) => {
-    return <button onClick={props.fetch}> Get a dog </button>
+  return <button onClick={props.fetch}> Get a dog </button>;
 };
 
-export default DogGallery; 
+export default DogGallery;
