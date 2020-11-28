d0f737af44b4cf7d4c9269bc2ad0d4ebe3d2c94e
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-28 14:24:38 +0100

    added hacjyoueweather in week3 folder

---
+++ week3/project/hackyourweather/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CityWeather from "./componenets/cities";
+
+function App() {
+  return (
+    <div>
+      <CityWeather />
+    </div>
+  );
+}
+
+export default App;
