05947d300060b09ab44ded2cd1ae337f907343d6
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-12-01 22:06:44 +0100

    updated app.js addde forecast componenet in it

--- week4/project/hackyourweather/src/App.js
+++ week4/project/hackyourweather/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import CityWeather from './componenets/cities';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CityWeather from "./componenets/cities";
+import Forecast from "./componenets/forecast";
 
 function App() {
   return (
-    <div>
-      <CityWeather />
+    <div id="app">
+      <h1 id="weather">Weather</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={CityWeather} />
+          <Route path="/:cityId" component={Forecast} />
+        </Switch>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
